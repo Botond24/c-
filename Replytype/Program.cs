616169bae4133cdbabf5cdc9025a69b3b5440d88
Botond24 <szihalmi.botond@gmail.com>
2020-10-02 19:40:09 +0200

    replytype

---
+++ Replytype/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Replytype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            var name = Console.ReadLine();
+        }
+    }
+}
